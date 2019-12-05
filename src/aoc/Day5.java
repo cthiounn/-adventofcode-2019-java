03a1081ccfb709a3271c9cf494afe607059c93c6
Conrad Thiounn <conrad.thiounn@insee.fr>
2019-12-05 16:22:25 +0100

    refactor day5

--- src/aoc/Day5.java
+++ src/aoc/Day5.java
@@ -7,154 +7,72 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Day5 {
-	static int input = 1;
 
 	public static void main(String[] args) throws IOException {
 
 		long timeStart = System.currentTimeMillis();
 		List<String> input = Files.readAllLines(Paths.get("src/main/resources/day5-input.file"));
 
-//		part1(input, 1);
-//		System.out.println(input);
+		part1(input, 1);
 		part1(input, 5);
-		System.out.println(input);
 		System.out.println("runned time : " + (System.currentTimeMillis() - timeStart) + " ms");
 
 	}
 
 	public static void part1(List<String> lines, int beginInput) {
-		int[] numbers = Arrays.asList(lines.get(0).split(",")).stream().mapToInt(Integer::parseInt).toArray();
-		input = beginInput;
+		int[] n = Arrays.asList(lines.get(0).split(",")).stream().mapToInt(Integer::parseInt).toArray();
 		int i = 0;
-		int value2 = 0;
-		int value3 = 0;
+		int r2 = 0;
+		int r3 = 0;
+		int r4 = 0;
+		int p1 = 0;
+		int p2 = 0;
+		int c = 0;
 		boolean stop = false;
 		while (!stop) {
-			System.out.println("---------------");
-			System.out.println("i=" + i);
-			System.out.println(
-					"c=" + numbers[i] + ";" + numbers[i + 1] + ";" + numbers[i + 2] + ";" + numbers[i + 3] + ";");
-			String command = pad(numbers[i]);
-			String param1 = command.substring(2, 3);
-			if ("1".equals(param1)) {
-				value2 = numbers[i + 1];
-
-				System.out.println("direct p1=" + value2);
-			} else if ("0".equals(param1) && command.endsWith("03")) {
-
-				value2 = numbers[i + 1];
-			} else {
-
-				value2 = numbers[numbers[i + 1]];
-				System.out.println("passive p1=" + value2);
-			}
-			String param2 = command.substring(1, 2);
-			if ("1".equals(param2)) {
-				value3 = numbers[i + 2];
-				System.out.println("direct p2=" + value3);
-			} else if ("0".equals(param1) && (command.endsWith("03") || command.endsWith("04"))) {
-				value3 = 0;
-
-				System.out.println("ignore p2=" + value3);
-			} else {
-
-				value3 = numbers[numbers[i + 2]];
-				System.out.println("passive p2=" + value2);
-			}
-			String param3 = command.substring(0, 1);
-			if (command.endsWith("01")) {
-				System.out.println("add");
-				System.out.println("value2=" + value2);
-				System.out.println("value3=" + value3);
-
-				System.out.println("b=" + numbers[numbers[i + 3]]);
-				calculate(1, value2, value3, numbers[i + 3], numbers);
-
-				System.out.println("a=" + numbers[numbers[i + 3]]);
+			r2 = i + 1 < n.length ? n[i + 1] : 0;
+			r3 = i + 2 < n.length ? n[i + 2] : 0;
+			r4 = i + 3 < n.length ? n[i + 3] : 0;
+			c = n[i] % 10;
+			p1 = n[i] % 1000 >= 100 || c == 3 || c == 9 ? r2 : n[r2];
+			p2 = n[i] % 10000 >= 1000 ? r3 : c == 3 || c == 4 || c == 9 ? 0 : n[r3];
+			if (c == 1) {
+				n[r4] = p1 + p2;
 				i += 4;
-			} else if (command.endsWith("02")) {
-
-				System.out.println("mult");
-
-				System.out.println("value2=" + value2);
-				System.out.println("value3=" + value3);
-
-				System.out.println("b=" + numbers[numbers[i + 3]]);
-				calculate(2, value2, value3, numbers[i + 3], numbers);
-
-				System.out.println("a=" + numbers[numbers[i + 3]]);
+			} else if (c == 2) {
+				n[r4] = p1 * p2;
 				i += 4;
-			} else if (command.endsWith("03")) {
-
-				System.out.println("write input");
-				calculate(3, value2, 0, 0, numbers);
+			} else if (c == 3) {
+				n[r2] = beginInput;
 				i += 2;
-			} else if (command.endsWith("04")) {
-
-				System.out.println("store input");
-				calculate(4, value2, 0, 0, null);
+			} else if (c == 4) {
+				beginInput = p1;
 				i += 2;
-			} else if (command.endsWith("05")) {
-				if (calculate(5, value2, 0, 0, null)) {
-					i = value3;
+			} else if (c == 5) {
+				if (p1 != 0) {
+					i = p2;
 				} else {
 					i += 3;
 				}
-			} else if (command.endsWith("06")) {
-
-				if (calculate(6, value2, 0, 0, null)) {
-					i = value3;
+			} else if (c == 6) {
+				if (p1 == 0) {
+					i = p2;
 				} else {
 					i += 3;
 				}
-			} else if (command.endsWith("07")) {
-				calculate(7, value2, value3, numbers[i + 3], numbers);
+			} else if (c == 7) {
+				n[r4] = p1 < p2 ? 1 : 0;
 				i += 4;
-			} else if (command.endsWith("08")) {
-				calculate(8, value2, value3, numbers[i + 3], numbers);
+			} else if (c == 8) {
+				n[r4] = p1 == p2 ? 1 : 0;
 				i += 4;
-			} else if (command.endsWith("99")) {
+			} else if (c == 9) {
 				stop = true;
 			} else {
-				System.out.println("? " + command);
+				System.out.println("? " + n[i]);
 				stop = true;
 			}
-			System.out.println(input);
-
 		}
-
-	}
-
-	private static String pad(int i) {
-		String st = i + "";
-		String ret = st;
-		for (int j = 0; j < 5 - st.length(); j++) {
-			ret = "0" + ret;
-		}
-		System.out.println("ret=" + ret);
-		return ret;
-	}
-
-	private static boolean calculate(int value1, int value2, int value3, int value4, int[] numbers) {
-		if (value1 == 1) {
-			numbers[value4] = value2 + value3;
-		} else if (value1 == 2) {
-			numbers[value4] = value2 * value3;
-		} else if (value1 == 3) {
-			numbers[value2] = input;
-		} else if (value1 == 4) {
-			input = value2;
-		} else if (value1 == 7) {
-			numbers[value4] = value2 < value3 ? 1 : 0;
-		} else if (value1 == 8) {
-			numbers[value4] = value2 == value3 ? 1 : 0;
-		} else if (value1 == 99) {
-			return true;
-		} else if (value1 == 5) {
-			return value2 != 0;
-		} else if (value1 == 6) {
-			return value2 == 0;
-		}
-		return false;
+		System.out.println(beginInput);
 	}
 }
