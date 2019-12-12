ad33f774baaa63d809c14a4d1ba0ec772bf221b5
cthiounn <conrad.thiounn@insee.fr>
2019-12-12 09:20:04 +0100

    day12 refactor

--- src/aoc/Day12.java
+++ src/aoc/Day12.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,9 +23,6 @@ public class Day12 {
 	}
 
 	public static void part1(List<String> lines) {
-		Set<String> hs = new HashSet<>();
-		Map<Long, Set<String>> registry = new HashMap<>();
-		int twoNumber = 0;
 		Set<String> xString = new HashSet<>();
 		Set<String> yString = new HashSet<>();
 		Set<String> zString = new HashSet<>();
@@ -53,27 +48,24 @@ public class Day12 {
 		int j = 0;
 		while (true) {
 			// velocity
-//			System.out.println("step i=" + j);
 			for (Moon m : Moon.allMoon) {
-//				System.out.println(m);
 				for (Moon m2 : Moon.allMoon) {
-					if (m.id == m2.id) {
-
+					if (m.getId() == m2.getId()) {
 					} else {
-						if (m.x < m2.x) {
-							m.vx += 1;
-						} else if (m.x > m2.x) {
-							m.vx += -1;
+						if (m.getX() < m2.getX()) {
+							m.setVx(m.getVx() + 1);
+						} else if (m.getX() > m2.getX()) {
+							m.setVx(m.getVx() - 1);
 						}
-						if (m.y < m2.y) {
-							m.vy += 1;
-						} else if (m.y > m2.y) {
-							m.vy += -1;
+						if (m.getY() < m2.getY()) {
+							m.setVy(m.getVy() + 1);
+						} else if (m.getY() > m2.getY()) {
+							m.setVy(m.getVy() - 1);
 						}
-						if (m.z < m2.z) {
-							m.vz += 1;
-						} else if (m.z > m2.z) {
-							m.vz += -1;
+						if (m.getZ() < m2.getZ()) {
+							m.setVz(m.getVz() + 1);
+						} else if (m.getZ() > m2.getZ()) {
+							m.setVz(m.getVz() - 1);
 						}
 					}
 				}
@@ -86,66 +78,36 @@ public class Day12 {
 				System.out.println(Moon.total());
 			}
 
-//			long hssHC = Moon.hCode().hashCode();
-//			String hss = Moon.hCode();
-//			if (registry.containsKey(hssHC)) {
-//				if (registry.get(hssHC).contains(hss)) {
-//					break;
-//				} else {
-//					registry.get(hssHC).add(hss);
-//				}
-//			} else {
-//				Set<String> hs2 = new HashSet<>();
-//				hs2.add(hss);
-//				registry.put(hssHC, hs2);
-//			}
-			String xcoord = Moon.allMoon.stream().map(e -> e.x + ";" + e.vx).collect(Collectors.toList()).toString();
+			String xcoord = Moon.allMoon.stream().map(e -> e.getX() + ";" + e.getVx()).collect(Collectors.toList())
+					.toString();
 			if (xString.contains(xcoord)) {
 				xInt.add(j);
-				twoNumber++;
 			} else {
 				xString.add(xcoord);
 			}
-			String ycoord = Moon.allMoon.stream().map(e -> e.y + ";" + e.vy).collect(Collectors.toList()).toString();
+			String ycoord = Moon.allMoon.stream().map(e -> e.getY() + ";" + e.getVy()).collect(Collectors.toList())
+					.toString();
 			if (yString.contains(ycoord)) {
 				yInt.add(j);
-				twoNumber++;
 			} else {
 				yString.add(ycoord);
 			}
-			String zcoord = Moon.allMoon.stream().map(e -> e.z + ";" + e.vz).collect(Collectors.toList()).toString();
+			String zcoord = Moon.allMoon.stream().map(e -> e.getZ() + ";" + e.getVz()).collect(Collectors.toList())
+					.toString();
 			if (zString.contains(zcoord)) {
 				zInt.add(j);
-				twoNumber++;
 			} else {
 				zString.add(zcoord);
 			}
 
 			if (!xInt.isEmpty() && !yInt.isEmpty() && !zInt.isEmpty()) {
-				System.out.println(xInt.get(0));
-				System.out.println(yInt.get(0));
-				System.out.println(zInt.get(0));
 				break;
 			}
 
 			j++;
 		}
-//		Long l = new Long(xInt.get(0) * yInt.get(0));
-//		Long ll = l / gcdByEuclidsAlgorithm(xInt.get(0), yInt.get(0));
-//
-//		Long l2 = new Long(ll * zInt.get(0));
-//
-//		Long ll2 = l2 / gcdByEuclidsAlgorithm(zInt.get(0), l2);
-//		System.out.println(ll2);
 
 		System.out.println(lcm(xInt.get(0), lcm(yInt.get(0), zInt.get(0))));
-	}
-
-	static int gcdByEuclidsAlgorithm(int n1, int n2) {
-		if (n2 == 0) {
-			return n1;
-		}
-		return gcdByEuclidsAlgorithm(n2, n1 % n2);
 	}
 
 	private static long gcd(long a, long b) {
@@ -163,14 +125,14 @@ public class Day12 {
 }
 
 class Moon {
-	int id = 0;
-	int x = 0;
-	int y = 0;
-	int z = 0;
-	int vx = 0;
-	int vy = 0;
-	int vz = 0;
-	static List<Moon> allMoon = new ArrayList<Moon>();
+	private int id = 0;
+	private int x = 0;
+	private int y = 0;
+	private int z = 0;
+	private int vx = 0;
+	private int vy = 0;
+	private int vz = 0;
+	public static List<Moon> allMoon = new ArrayList<Moon>();
 
 	public Moon(int id, int x, int y, int z, int vx, int vy, int vz) {
 		this.id = id;
@@ -190,12 +152,10 @@ class Moon {
 	}
 
 	public int cinetic() {
-
 		return Math.abs(vx) + Math.abs(vy) + Math.abs(vz);
 	}
 
 	public int pot() {
-
 		return Math.abs(x) + Math.abs(y) + Math.abs(z);
 	}
 
@@ -203,14 +163,66 @@ class Moon {
 		return allMoon.stream().mapToLong(x -> x.cinetic() * x.pot()).sum();
 	}
 
-	public static String hCode() {
-		return allMoon.toString();
-	}
-
 	@Override
 	public String toString() {
 		return "Moon [id=" + id + ", x=" + x + ", y=" + y + ", z=" + z + ", vx=" + vx + ", vy=" + vy + ", vz=" + vz
 				+ "]";
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	public int getVx() {
+		return vx;
+	}
+
+	public void setVx(int vx) {
+		this.vx = vx;
+	}
+
+	public int getVy() {
+		return vy;
+	}
+
+	public void setVy(int vy) {
+		this.vy = vy;
+	}
+
+	public int getVz() {
+		return vz;
+	}
+
+	public void setVz(int vz) {
+		this.vz = vz;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
